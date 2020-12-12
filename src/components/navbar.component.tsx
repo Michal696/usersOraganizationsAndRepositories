0c3b07fbfb019308d023cd11127239c40093d67f
Michal696 <michal.sajdik@sap.com>
2020-12-12 11:35:32 +0100

    Remove NavBar

--- src/components/navbar.component.tsx
+++ src/components/navbar.component.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
-import Navbar from 'react-bootstrap/Navbar'
+// import Navbar from 'react-bootstrap/Navbar'
 import {Component} from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 
 export default class NavbarComponent extends Component {
   public render() {
     return (
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home">GitHub exercise</Navbar.Brand>
-        </Navbar>
+        // <Navbar bg="light" expand="lg">
+        //   <Navbar.Brand href="#home">GitHub exercise</Navbar.Brand>
+        // </Navbar>
+        ''
     )
   }
 }
